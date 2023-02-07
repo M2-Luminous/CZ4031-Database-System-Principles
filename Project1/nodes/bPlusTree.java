9bf0f0e9c22632e5276cc244ede444d1266ed701
Minjuan Luo <luom@tcd.ie>
2023-02-07 16:48:03 +0800

    more updates

--- Project1/nodes/bPlusTree.java
+++ Project1/nodes/bPlusTree.java
@@ -94,13 +94,118 @@ public class bPlusTree {
     public void splitLeaf(leafNode old, int key, Address address) {
         int keys[] = new int[maxKeys + 1];
         Address addresses[] = new Address[maxKeys + 1];
-        leafNode leaves = new leafNode();
+        leafNode LEAF = new leafNode();
         int i;
 
         //get sorted lists of keys and addresses
-        for(i = 0; i< maxKeys; i++) {
+        for(i = 0; i < maxKeys; i ++) {
             keys[i] = old.getOneKey(i);
             addresses[i] = old.getOneRecord(i);
         }
+
+        for(i = maxKeys - 1; i >= 0; i --) {
+            if(keys[i] <= key) {
+                i ++;
+                keys[i] = key;
+                addresses[i] = address;
+                break;
+            }
+            keys[i + 1] = keys[i];
+            addresses[i + 1] = addresses[i];
+        }
+
+        //cleaning old leafnode values
+        old.splitPrep();
+
+        //putting the keys and addresses into the two leafnodes
+        for(i = 0; i < leafMinKeys; i ++) {
+            old.addRecord(keys[i], addresses[i]);
+        }
+
+        for(i = leafMinKeys; i < maxKeys + 1; i ++) {
+            LEAF.addRecord(keys[i], addresses[i]);
+        }
+
+        //old leafnode to point to new leafnode
+        //new leafnode point to next leafnode 
+        LEAF.setNext(old.getNext());
+        old.setNext(LEAF);
+
+        //setting parents for LEAF
+        if(old.getIsRoot()) {
+            parentNode newRoot = new parentNode();
+            old.setIsRoot(false);
+            newRoot.setIsRoot(true);
+            newRoot.addChild(old);
+            newRoot.addChild(LEAF);
+            root = newRoot;
+            height++;
+        }
+        else if(old.getParent().getAllKey().size() < maxKeys) {
+            old.getParent().addChild(LEAF);
+        }
+        else {
+            splitParent(old.getParent(), LEAF);
+        }
+
+        //update node count
+        nodeCount ++;
+    }
+
+    //split parent node
+    public void splitParent(parentNode parent, Node child) {
+        Node children[] = new Node[maxKeys + 2];
+        int keys[] = new int[maxKeys + 2];
+        int key = child.findSmallestKey();
+        parentNode PARENT = new parentNode();
+
+        //getting sorted lists of keys and children
+        for(int i = 0; i < maxKeys + 1; i ++) {
+            children[i] = parent.getChild(i);
+            keys[i] = children[i].findSmallestKey();
+        }
+
+        for(int i = maxKeys; i >= 0; i --) {
+            if(keys[i] <= key) {
+                i++;
+                keys[i] = key;
+                children[i] = child;
+                break;
+            }
+
+            keys[i + 1] = keys[i];
+            children[i + 1] = children[i];
+        }
+
+        //cleaning old parent values
+        parent.splitPrep();
+
+        //putting children into two parent nodes
+        for(int i = 0; i < parentMinKeys + 2; i ++) {
+            parent.addChild(children[i]);
+        }
+        for(int i = parentMinKeys + 2; i < maxKeys + 2; i ++) {
+            PARENT.addChild(children[i]);
+        }
+
+        //setting new parent node
+        if(parent.getIsRoot()) {
+            parentNode newRoot = new parentNode();
+            parent.setIsRoot(false);
+            newRoot.setIsRoot(true);
+            newRoot.addChild(parent);
+            newRoot.addChild(PARENT);
+            root = newRoot;
+            height ++;
+        }
+        else if(parent.getParent().getAllKey().size() < maxKeys) {
+            parent.getParent().addChild(PARENT);
+        }
+        else {
+            splitParent(parent.getParent(), PARENT);
+        }
+
+        //update node count
+        nodeCount ++;
     }
 }
