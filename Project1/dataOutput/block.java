cd10b41eca8806e288e8844a954ff07a70e08830
prolude1 <79588462+prolude1@users.noreply.github.com>
2023-02-09 21:49:12 +0800

    Added entry logic that takes care of entry logic. Working on block.java logic now.

--- Project1/dataOutput/block.java
+++ Project1/dataOutput/block.java
@@ -1,5 +1,15 @@
-package dataOutput;
+package Project1.dataOutput;
 
 public class block {
-    
+    private int max_num_records;
+    public int current_num_records;
+    private entry[] all_records;
+
+    public block(int size){
+        this.max_num_records=size/entry.get_storage();
+        this.current_num_records=0;
+        this.all_records=new entry[this.max_num_records];
+    }
+
+
 }
---
+++ Project1/dataOutput/entry.java
@@ -0,0 +1,35 @@
+package Project1.dataOutput;
+
+public class entry {
+    private String tconst;
+    private int numVotes;
+    private float averageRating;
+
+    public entry(String tconst,int numVotes,float averageRating){
+        this.averageRating=averageRating;
+        this.tconst=tconst;
+        this.numVotes=numVotes;
+    }
+
+    public static int get_storage(){
+        return 10+4+4;
+    }
+    public String gettconst(){
+        return tconst;
+    }
+    public int getnumVotes(){
+        return numVotes;
+    }
+    public float getaverageRating(){
+        return averageRating;
+    }
+    public void setaverageRating(float new_averageRating){
+        this.averageRating=new_averageRating;
+    }
+    public void setnumVotes(int new_numVotes){
+        this.numVotes=new_numVotes;
+    }
+    public void setaverageRating(String new_tconst){
+        this.tconst=new_tconst;
+    }
+}
