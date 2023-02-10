a3a5fa5d13b9dd74f16c16c3043398cf4da6dc40
prolude1 <79588462+prolude1@users.noreply.github.com>
2023-02-10 12:01:24 +0800

    Updated storage

--- Project1/dataOutput/address.java
+++ Project1/dataOutput/address.java
@@ -1,5 +1,17 @@
 package Project1.dataOutput;
 
 public class address {
-    
+    private block block_data;
+    private int block_ID;
+
+    public address(int block_ID){
+        this.block_ID=block_ID;
+        this.block_data=new block(200);
+    }
+    public block get_block(){
+        return this.block_data;
+    }
+    public int get_block_ID(){
+        return this.block_ID;
+    }
 }
--- Project1/dataOutput/block.java
+++ Project1/dataOutput/block.java
@@ -17,11 +17,42 @@ public class block {
         return this.max_num_records>=this.current_num_records;
     }
 
-    public int insertrecord(){
-
+    public int insertrecord(entry record){
+        if (this.checkinsert()){
+            for(int i=0;i<this.all_records.length;i++){
+                if (this.all_records[i]==null){
+                    this.all_records[i]=record;
+                    this.current_num_records++;
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 
-    public deleterecord(){
-        
+    public boolean deleterecord(int index){
+        if (this.all_records[index]!=null){
+            this.all_records[index]=null;
+            this.current_num_records--;
+            return true;
+        }
+        return false;
+    }
+
+    public entry getrecord(int index){
+        return this.all_records[index];
+    }
+
+    public entry getrecordat(int index){
+        return this.all_records[index];
+    }
+
+    public void printblock(){
+        System.out.println("Printing block:");
+        System.out.printf("Current number of records: %d. All records:%n",this.current_num_records);
+        for (int i=0; i<current_num_records;i++){
+            this.all_records[i].printentry();
+        }
+        System.out.println("Finished printing block");
     }
 }
