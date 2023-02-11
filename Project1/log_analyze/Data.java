ae22ad034eed59153cceffaf7d06595a30bbcbb3
liul4 <liul4@tcd.ie>
2023-02-11 17:45:46 +0800

    update data

---
+++ Project1/log_analyze/Data.java
@@ -0,0 +1,33 @@
+package log_analyze;
+
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Data {
+    public static List<Record> records = new ArrayList<>();;
+    //private static final String TAG = "Data";
+    //public static List<Record> readData() throws IOException {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader;
+        try{
+            reader = new BufferedReader(new FileReader("data.tsv"));
+            reader.readLine();
+			String line = reader.readLine();
+            while (line != null) {
+                String[] words = line.split("\\t");
+                Record record = new Record(words[0], words[1], words[2]);
+                records.add(record);
+                line = reader.readLine();
+            }
+        }
+        catch (FileNotFoundException e) {	
+			e.printStackTrace();
+		}
+    }
+    public List<Record> getRecord(){
+        return records;
+    }
+}
